3a8670d4f2452bd15d96ae4a717948c269ad69fc
wisekim <lwisekiml@naver.com>
2021-01-04 22:52:43 +0900

    print integer

--- HelloWorld/src/helloworld.java
+++ HelloWorld/src/helloworld.java
@@ -2,8 +2,9 @@
 public class helloworld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			System.out.println("Hello World");
+		int num = 5;
+		System.out.println("Hello World");
+		System.out.println(num);
 	}
 
 }
