f5ad3bc1a7f60c367ce7a6bf410b0baf94ddbd7f
wisekim <lwisekiml@naver.com>
2021-01-05 22:50:25 +0900

    boolean test

--- HelloWorld/src/helloworld.java
+++ HelloWorld/src/helloworld.java
@@ -3,8 +3,17 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		int num = 5;
+		boolean exp = true;
+		
 		System.out.println("Hello World");
 		System.out.println(num);
+		
+		if (exp)
+			System.out.println("true");
+		else
+			System.out.println("false");
+		
+		
 	}
 
 }
