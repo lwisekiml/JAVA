e1d92170ac90aefb0474c711e85b8f1be3e684c1
wisekim <lwisekiml@naver.com>
2021-01-07 22:50:23 +0900

    for test

--- HelloWorld/src/helloworld.java
+++ HelloWorld/src/helloworld.java
@@ -8,17 +8,24 @@ public class helloworld {
 		System.out.println("Hello World");
 		System.out.println(num);
 		
+		System.out.println("--- if ---");
 		if (exp)
 			System.out.println("true");
 		else
 			System.out.println("false");
 		
+		System.out.println("--- while ---");
 		int i = 1;
 		while(true) {
 			System.out.println(i);
 			i++;
 			if(i > 5)
 				break;
+		}
+		
+		System.out.println("--- for ---");
+		for(i = 5; i > 0; i--) {
+			System.out.println(i);
 		}
 		
 		
