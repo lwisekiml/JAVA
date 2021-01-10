36ad9e1fc6d54c4f0f319521b80bef7696cbdd51
wisekim <lwisekiml@naver.com>
2021-01-10 23:37:30 +0900

    continue test

--- HelloWorld/src/helloworld.java
+++ HelloWorld/src/helloworld.java
@@ -4,16 +4,16 @@ public class helloworld {
 	public static void main(String[] args) {
 		int num = 5;
 		boolean exp = true;
-		
+
 		System.out.println("Hello World");
 		System.out.println(num);
-		
+
 		System.out.println("--- if ---");
 		if (exp)
 			System.out.println("true");
 		else
 			System.out.println("false");
-		
+
 		System.out.println("--- while ---");
 		int i = 1;
 		while(true) {
@@ -22,24 +22,32 @@ public class helloworld {
 			if(i > 5)
 				break;
 		}
-		
+
 		System.out.println("--- for ---");
 		for(i = 5; i > 0; i--) {
 			System.out.println(i);
 		}
-		
+
 		System.out.println("-- Condition Operator ---");
 		int b = (5 < 4) ? 50 : 40;
 		System.out.println(b);
-		
-		System.out.println("-- bitwise operator"
-				+ " ---");
+
+		System.out.println("-- bitwise operator ---");
 		int num1 = 8, num2 = -8;
 		System.out.println("~ 연산자에 의한 결과 : "+ ~num1);
 		System.out.println("<< 연산자에 의한 결과 : "+ (num1 << 2));
 		System.out.println(">> 연산자에 의한 결과 : "+ (num2 >> 2));
 		System.out.println(">>> 연산자에 의한 결과 : "+ (num1 >>> 2));
 		System.out.println(">>> 연산자에 의한 결과 : "+ (num2 >>> 2));
-		
+
+		System.out.println("-- continue ---");
+		for (i = 1; i <= 100; i++) {
+			if (i % 5 == 0 || i % 7 == 0) {
+				System.out.println(i);
+			} else {
+				continue;
+			}
+		}
+
 	}
 }
