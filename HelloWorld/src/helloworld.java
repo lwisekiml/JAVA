4a0ddaf66ec3e97b2ca0b44977167930816d4382
wisekim <lwisekiml@naver.com>
2021-01-22 18:45:03 +0900

    recursive call test

--- HelloWorld/src/helloworld.java
+++ HelloWorld/src/helloworld.java
@@ -229,5 +229,20 @@ public class helloworld {
 		overloading.display(10,20);
 		overloading.display(10,3.14);
 		overloading.display(10,'a');
+		
+		System.out.println("\n--- recursive call ---");
+		System.out.println(recursiveSum(5));
+	}
+	
+	static int recursiveSum(int n) {
+
+	    if (n == 1) {                 // n이 1이면, 그냥 1을 반환함.
+
+	        return 1;
+
+	    }
+
+	    return n + recursiveSum(n-1); // n이 1이 아니면, n을 1부터 (n-1)까지의 합과 더한 값을 반환함.
+
 	}
 }
