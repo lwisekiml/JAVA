14cacc162d5072b0423ca1e2fe10399c219fe31c
wisekim <lwisekiml@naver.com>
2021-01-23 23:04:30 +0900

    private access modifier test

--- HelloWorld/src/helloworld.java
+++ HelloWorld/src/helloworld.java
@@ -35,6 +35,13 @@ class Car {                    // 클래스 이름
     static void display(int num1) { System.out.println(num1); }
     static void display(int num1, int num2) { System.out.println(num1 * num2); }
     static void display(int num1, double num2) { System.out.println(num1 + num2); }
+    
+    public void gcs(){
+    	System.out.println(getCurrentSpeed());
+    }
+    private int getCurrentSpeed() {                  // private 메소드
+        return this.currentSpeed;
+    }
 }
 
 public class helloworld {
@@ -232,17 +239,16 @@ public class helloworld {
 		
 		System.out.println("\n--- recursive call ---");
 		System.out.println(recursiveSum(5));
+		
+		System.out.println("--- access modifier ---");
+		System.out.println("--- private ");
+		myCar.gcs();
 	}
 	
 	static int recursiveSum(int n) {
-
 	    if (n == 1) {                 // n이 1이면, 그냥 1을 반환함.
-
 	        return 1;
-
 	    }
-
 	    return n + recursiveSum(n-1); // n이 1이 아니면, n을 1부터 (n-1)까지의 합과 더한 값을 반환함.
-
 	}
 }
