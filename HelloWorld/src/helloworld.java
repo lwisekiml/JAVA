f9bdac3ab2133c94981ee8a82b9b8f351467fa63
wisekim <lwisekiml@naver.com>
2021-01-25 23:36:13 +0900

    final test

--- HelloWorld/src/helloworld.java
+++ HelloWorld/src/helloworld.java
@@ -1,5 +1,12 @@
 import java.util.Arrays;
 
+final class Car {                    // 이 클래스는 상속을 통해 서브 클래스를 생성할 수 없음.
+    final int VAR;                   // 이 필드는 상수화되어 값을 변경할 수 없음.
+    final void brake() {             // 이 메소드는 오버라이딩을 통해 재정의할 수 없음.
+        final double MAX_NUM = 10.2; // 이 지역 변수는 상수화되어 값을 변경할 수 없음.
+    }
+}
+
 class Car {                    // 클래스 이름
     private String modelName;  // 필드
     private int modelYear;     // 필드
