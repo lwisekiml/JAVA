6d83f6992618e1088aae47346648c08ecb31e2c2
wisekim <lwisekiml@naver.com>
2021-01-26 20:25:12 +0900

    static test

--- HelloWorld/src/helloworld.java
+++ HelloWorld/src/helloworld.java
@@ -1,9 +1,20 @@
 import java.util.Arrays;
 
+// final
 final class Car {                    // 이 클래스는 상속을 통해 서브 클래스를 생성할 수 없음.
     final int VAR;                   // 이 필드는 상수화되어 값을 변경할 수 없음.
     final void brake() {             // 이 메소드는 오버라이딩을 통해 재정의할 수 없음.
         final double MAX_NUM = 10.2; // 이 지역 변수는 상수화되어 값을 변경할 수 없음.
+    }
+}
+
+// static
+class Car {
+    static int var;       // 클래스 필드(static 변수)
+    static {              // static 초기화 블록
+    // 보통 클래스 필드의 초기화를 진행함.
+    }
+    static void brake() { // 클래스 메소드(static 메소드)
     }
 }
 
