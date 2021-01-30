48d938a71392cf29208c34c2520779464c16c634
wisekim <lwisekiml@naver.com>
2021-01-30 23:07:49 +0900

    필드 초기화 순서

--- HelloWorld/src/helloworld.java
+++ HelloWorld/src/helloworld.java
@@ -26,6 +26,26 @@ public class Member04 {
     }
 }
 
+/*
+ * 1. 클래스 변수 : 기본값 → 명시적 초기화 → 클래스 초기화 블록
+ * 2. 인스턴스 변수 : 기본값 → 명시적 초기화 → 인스턴스 초기화 블록 → 생성자
+ */
+class InitBlock {
+    static int classVar = 10;         // 클래스 변수의 명시적 초기화
+    int instanceVar = 10;             // 인스턴스 변수의 명시적 초기화
+    static { classVar = 20; }         // 클래스 초기화 블록을 이용한 초기화
+    { instanceVar = 20; }             // 인스턴스 초기화 블록을 이용한 초기화
+    InitBlock() { instanceVar = 30; } // 생성자를 이용한 초기화
+}
+
+public class Member05 {
+    public static void main(String[] args) {
+        System.out.println(InitBlock.classVar);
+        InitBlock myInit = new InitBlock();
+        System.out.println(myInit.instanceVar);
+    }
+}
+
 // static
 class Car_ {
     static int var;       // 클래스 필드(static 변수)
