8b4149d0105807b2bb0bd07752389ce587f56525
wisekim <lwisekiml@naver.com>
2021-02-25 23:50:02 +0900

    wrapper class

---
+++ TCPSCHOOL_12/src/WrapperTest.java
@@ -0,0 +1,27 @@
+/*
+Wrapper 클래스
+프로그램에 따라 기본 타입의 데이터를 객체로 취급해야 하는 경우
+(메소드의 인수로 객체 타입만이 요구되면, 기본 타입의 데이터를 그대로 사용할 수 없음)
+기본 타입의 데이터를 먼저 객체로 변환한 후 작업을 수행
+
+Wrapper class : 8개의 기본 타입에 해당하는 데이터를 객체로 포장해 주는 클래스
+
+각각의 타입에 해당하는 데이터를 인수로 전달받아, 해당 값을 가지는 객체로 만들어  줌
+java.lang 패키지에 포함되어 제공
+
+기본 타입에 대응하여 제공하고 있는 래퍼 클래스
+기본 타입	래퍼 클래스
+byte	Byte
+short	Short
+int		Integer
+long	Long
+float	Float
+double	Double
+char	Character
+boolean	Boolean
+*/
+public class WrapperTest {
+	public static void main(String[] args) {
+
+	}
+}
