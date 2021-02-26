77d0d817ab45794d2b270875aee217135b814e59
wisekim <lwisekiml@naver.com>
2021-02-26 23:55:02 +0900

    boxing unboxing

--- TCPSCHOOL_12/src/WrapperTest.java
+++ TCPSCHOOL_12/src/WrapperTest.java
@@ -1,5 +1,5 @@
 /*
-Wrapper 클래스
+1. Wrapper 클래스
 프로그램에 따라 기본 타입의 데이터를 객체로 취급해야 하는 경우
 (메소드의 인수로 객체 타입만이 요구되면, 기본 타입의 데이터를 그대로 사용할 수 없음)
 기본 타입의 데이터를 먼저 객체로 변환한 후 작업을 수행
@@ -19,6 +19,14 @@ float	Float
 double	Double
 char	Character
 boolean	Boolean
+
+래퍼 클래스(Wrapper class)는 산술 연산을 위해 정의된 클래스가 아니므로, 인스턴스에 저장된 값을 변경할 수 없습니다.
+단지, 값을 참조하기 위해 새로운 인스턴스를 생성하고, 생성된 인스턴스의 값만을 참조할 수 있습니다.
+
+2. 박싱(Boxing)과 언박싱(UnBoxing)
+박싱(Boxing)   : 기본 타입의 데이터를 래퍼 클래스의 인스턴스로 변환하는 과정
+언박싱(UnBoxing): 래퍼 클래스의 인스턴스에 저장된 값을 다시 기본 타입의 데이터로 꺼내는 과정
+
 */
 public class WrapperTest {
 	public static void main(String[] args) {
