5af004543dae826a408e6581b241cf3fc1c082b6
wisekim <lwisekiml@naver.com>
2021-03-08 22:32:23 +0900

    Exception class 1

--- TCPSCHOOL_14/src/ExceptionError.java
+++ TCPSCHOOL_14/src/ExceptionError.java
@@ -1,7 +1,8 @@
+import java.io.IOException;
 
 public class ExceptionError {
 	public static void main(String[] args) {
-/*
+		/*
 # 예외 처리
 # 오류(error)와 예외(exception)
 자바 문법에 맞지 않게 코드를 작성하고 컴파일하려고 하면, 자바 컴파일러는 문법 오류(syntax error)를 발생
@@ -42,7 +43,35 @@ catch 블록과 finally 블록은 선택적인 옵션으로 반드시 사용할 필요는 없음
 3. try / catch / ... / finally
 
 다른 제어문과는 달리 예외 처리문은 중괄호({})를 생략할 수 없음
-*/
-	}
+		 */
+		/*
+# 예외 클래스
+# Exception 클래스
+모든 예외의 조상 클래스가 되는 Exception 클래스는 크게 다음과 같이 구분할 수 있습니다.
 
+1. RuntimeException 클래스
+2. 그 외의 Exception 클래스의 자식 클래스
+
+RuntimeException 클래스를 상속받는 자식 클래스들은 주로 치명적인 예외 상황을 발생시키지 않는 예외들로 구성
+try / catch 문을 사용하기보다는 프로그램을 작성하면서 예외가 발생하지 않도록 주의를 기울이는 편이 좋음 
+
+그 외의 Exception 클래스에 속하는 자식 클래스들은 치명적인 예외 상황을 발생시키므로, 반드시 try / catch 문을 사용하여 예외를 처리해야만 함
+자바 컴파일러는 RuntimeException 클래스 이외의 Exception 클래스의 자식 클래스에 속하는 예외가 발생할 가능성이 있는 구문에는 반드시 예외를 처리하도록 강제하고 있음
+
+PrintStream 클래스의 write() 메소드를 사용하여 byte 타입 배열의 모든 요소를 출력하는 예제
+*/
+//		byte[] list = {'a', 'b', 'c'};
+//		System.out.write(list);
+
+// 위 예제에서는 write() 메소드에서 발생할 수 있는 IOException에 대한 예외를 처리하지 않았으므로, 컴파일 시 오류 발생
+// 다음 예제와 같이 try / catch 문을 사용하여 IOException에 대한 예외 처리까지 해 주어야만 컴파일 할 수 있음
+
+		byte[] list = {'a', 'b', 'c'};
+
+		try {
+			System.out.write(list);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
