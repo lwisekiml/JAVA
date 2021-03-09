8d7cb8d5c79066966e2d42fbed640eb5b8fde290
wisekim <lwisekiml@naver.com>
2021-03-09 21:34:24 +0900

    Exception class 2

--- TCPSCHOOL_14/src/ExceptionError.java
+++ TCPSCHOOL_14/src/ExceptionError.java
@@ -43,8 +43,8 @@ catch 블록과 finally 블록은 선택적인 옵션으로 반드시 사용할 필요는 없음
 3. try / catch / ... / finally
 
 다른 제어문과는 달리 예외 처리문은 중괄호({})를 생략할 수 없음
-		 */
-		/*
+*/
+/*
 # 예외 클래스
 # Exception 클래스
 모든 예외의 조상 클래스가 되는 Exception 클래스는 크게 다음과 같이 구분할 수 있습니다.
@@ -72,6 +72,82 @@ PrintStream 클래스의 write() 메소드를 사용하여 byte 타입 배열의 모든 요소를 출력
 			System.out.write(list);
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-	}
+		}	
+/*
+# 예외 처리의 계층 관계
+예외가 발생하면, try 블록과 가장 가까운 catch 블록부터 순서대로 검사
+여러 개의 catch 블록을 사용할 때는 Exception 클래스의 계층 관계에도 주의를 기울여야만 함
+*/
+try {
+    System.out.write(list);
+} catch (Exception e) {
+    e.printStackTrace();
+} catch (IOException e) {
+    e.printStackTrace();
+}
+/*
+위의 예에서 IOException이 발생하면, 자바는 첫 번째 catch 블록부터 순서대로 해당 예외를 처리할 수 있는지를 검사함
+그런데 IOException은 Exception의 자식 클래스이므로, 첫 번째 catch 블록에서도 IOException을 처리할 수 있음
+따라서 IOException을 비롯한 Exception 클래스의 자식 클래스에 해당하는 예외가 발생하면, 언제나 첫 번째 catch 블록에서만 처리함
+즉, catch 블록의 순서를 위의 예제처럼 작성하면, 두 번째 catch 블록은 영원히 실행되지 못함
+따라서 IOException만을 따로 처리하고자 한다면, 다음 예제처럼 catch 블록의 순서를 변경해야 함
+*/
+try {
+    System.out.write(list);
+} catch (IOException e) {
+    e.printStackTrace();
+} catch (Exception e) {
+    e.printStackTrace();
+}
+/*
+위의 예제에서 IOException이 발생하면, 첫 번째 catch 블록에서 해당 예외를 처리할 것임
+또한, IOException 외의 Exception 클래스의 자식 클래스에 해당하는 예외가 발생하면, 두 번째 catch 블록에서 처리될 것임
+범위가 더 좁은 예외를 처리하는 catch 블록을 먼저 명시하고, 범위가 더 넓은 예외를 처리하는 catch 블록은 나중에 명시해야만 정상적으로 해당 예외를 처리할 수 있음
+
+# 여러 예외 타입의 동시 처리
+Java SE 7부터는 '|' 기호를 사용하여 하나의 catch 블록에서 여러 타입의 예외를 동시에 처리할 수 있습니다.
+즉, Java SE 7부터는 위의 예제를 다음 예제처럼 표현할 수 있게 되었습니다.
+*/
+try {
+    this.db.commit();
+} catch (IOException | SQLException e) {
+    e.printStackTrace();
+}
+/*
+하지만 둘 이상의 예외 타입를 동시에 처리하는 catch 블록에서 매개변수로 전달받은 예외 객체는 묵시적으로 final 제어자를 가지게 됨
+catch 블록 내에서 해당 매개변수에는 어떠한 값도 대입할 수 없음
+
+# Throwable 클래스
+자바에서 Throwable 클래스는 모든 예외의 조상이 되는 Exception 클래스와 모든 오류의 조상이 되는 Error 클래스의 부모 클래스
+Throwable 타입과 이 클래스를 상속받은 서브 타입만이 자바 가상 머신(JVM)이나 throw 키워드에 의해 던져질 수 있음
+이 클래스에는 예외나 오류에 관한 다양한 정보를 확인할 수 있는 다음과 같은 메소드가 포함되어 있음
+
+메소드					설명
+String getMessage()		해당 throwable 객체에 대한 자세한 내용을 문자열로 반환함.
+void printStackTrace()	해당 throwable 객체와 표준 오류 스트림(standard error stream)에서 해당 객체의 스택 트레이스(stack trace)를 출력함.
+String toString()		해당 throwable 객체에 대한 간략한 내용을 문자열로 반환함.
+
+일부러 숫자를 0으로 나눠 ArithmeticException 오류를 발생시키는 예제
+발생한 오류에 대해 Throwable 메소드를 사용하여 발생한 오류에 대한 정보를 출력
+*/
+try {
+    System.out.println(5 / 0);
+} catch (ArithmeticException e) {
+    System.out.println("현재 발생한 예외 정보 : " + e.getMessage());
+}
+/*
+실행 결과
+현재 발생한 예외 정보 : / by zero
+*/
+
+/*
+# 자주 사용되는 예외 클래스
+자바에서 자주 사용되는 예외 클래스는 다음과 같습니다.
+
+클래스							설명
+ClassCastException				수행할 수 없는 타입 변환이 진행될 경우
+ArrayIndexOutOfBoundsException	배열에 잘못된 인덱스를 사용하여 접근할 경우
+NullPointerException			null 객체의 인스턴스 메소드를 호출하는 등의 경우
+ArithmeticException				산술 연산에서 정수를 0으로 나누는 등 연산을 수행할 수 없는 경우
+*/
 }
